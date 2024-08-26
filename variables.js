be8cb2a21d04669a77cc97d95c4567091eec02e2
Trivedi-800 <02trivedi@gmail.com>
2024-08-26 20:16:13 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,5 @@
+//variables in javascript
+let a = "ram"; //in current use .
+var b ="shyam";//  for old web_browsers 
+const c=20 //once the value  declared it  can't be changed again.
+console.table([a,b,c])

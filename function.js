f89712a2303654c9b253be2bf69e209682f7bac1
Trivedi-800 <02trivedi@gmail.com>
2024-08-31 21:28:35 +0530

    functions

---
+++ function.js
@@ -0,0 +1,26 @@
+// function saymyname(){
+// console.log("Abhishek")}
+// saymyname()
+
+// function addTwonumbers(n1,n2){
+// console.log(n1+n2)
+// }
+// addTwonumbers(55,85.5)//if no val is passed then NaN is output
+// addTwonumbers(55,"a") //here both argumnets are treated as strings
+
+// function newfun(n1,n2){
+//     console.log(n1+n2)
+// }
+// const result =  newfun(3,5)  //console will print 8 and result is undefined
+// console.log("Result:",result);
+
+// 
+function loginUserMessage(username){
+    if(username=== undefined){
+        console.log("please enter username");
+        return
+    }
+    return`${username} just logged in`
+}
+
+console.log(loginUserMessage("abhi"))

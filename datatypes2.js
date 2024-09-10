52368099b18f4903cf40f1d54c0ac25e3f0d1bc1
Trivedi-800 <02trivedi@gmail.com>
2024-09-10 21:34:23 +0530

    datatypes 2 part

---
+++ datatypes2.js
@@ -0,0 +1,26 @@
+// let scr = "33";
+//console.log(typeof scr);
+//console.log(typeof scr)
+// let valueInnumber = Number(scr)
+// console.log(typeof valueInnumber);
+
+
+// let scr = "33abc";
+// console.log(typeof scr); 
+// let valueInnumber = Number(scr)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);//althiugh here conversion of datatype is possible but no value as a o/p.
+
+
+// let scr = null;//in case of undefined  it will show NaN as output .
+//  console.log(typeof scr);
+// let valueInnumber = Number(scr)
+//  console.log(typeof valueInnumber);
+//  console.log(valueInnumber)
+
+// let isloggedIn=1
+// let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn)
+
+//" " =>false
+//"abhishek" =>true

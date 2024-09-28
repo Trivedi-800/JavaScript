2fcd618a0ca426ca407c4683cc90e186e77d5c58
Trivedi-800 <02trivedi@gmail.com>
2024-09-28 23:35:59 +0530

    new practice

---
+++ array_methods.js
@@ -0,0 +1,89 @@
+//  let arr=[10,20,30,40];
+ //console.log(arr.unshift(50,60,70))
+// console.log(arr.push(120));
+
+// arr.splice(2,1)
+// console.log(arr)
+
+// arr2=['raju','shyam','jahangir']
+// let arr3=arr2.concat(arr)
+// console.log(arr3)
+
+// let text="Abhishek Trivedi is my name ."
+// let array=text.split(' ')
+// console.log(array)
+
+// let array=[ 'Abhishek', 'Trivedi', 'is', 'my', 'name', '.' ]
+// let joint=array.join('- ')
+// console.log(joint)
+
+//   arr=["abhishek","trivedi","aman","savita"]
+//    arr2 = arr.slice(1,2)
+//   console.log(arr2)
+
+// let fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// let citrus = fruits.slice(1, 3)
+// console.log(citrus)
+
+// let z=[1,2,3,4,5,6]
+//  x =z.slice(4)
+//  x=z.slice(-6,-3)
+// console.log(x)
+
+// let z=[1,2,3,4,5,6]
+// b=z.indexOf(4)
+// console.log(b)
+
+// let arr= [2,4,8,10]
+// let updatedArr = arr.map(val=> val+2)
+// console.log(arr);
+// document.write(updatedArr);
+
+
+//  arr=[10,20,30,40,50]
+//  document.write(arr.map(myfun))
+//  function myfun(arr){
+//     return arr*100
+// }
+
+// const numbers = [4, 9, 16, 25];
+// const newArr = numbers.map(Math.sqrt)
+// console.log(newArr)
+
+// let arr = [2, 4, 8, 10];
+// let updatedArr = arr.filter(i=>i<5);
+// console.log(arr);
+// console.log(updatedArr);
+
+// arr=[10,20,30,40,50]
+//  document.write(arr.filter(myfun))
+//   function myfun(arr){
+//          return arr>30
+//         }
+
+// let arr=[20,50,80,90,70,50]
+// let updatedArr=arr.reduce((i,j)=>i+j,0);
+// console.log(updatedArr)
+
+// let age =[18,21,33,25,27,29]
+// document.write(age.find(checkage))
+// function checkage(age){
+//     return age>18;
+// }
+
+// const arr = [1, 2, 3, 4, 5];
+
+// arr.forEach((i,item) => {
+//     console.log(item+i);
+// });
+
+// let students = [1,2,3];
+// // using forEach
+// students.forEach(myFunction);
+// function myFunction(item) {
+//      console.log(item);
+// }
+
+// arr=[200,50,600,70,80]
+// document.write(arr.sort((a,b)=>{return a-b})+"<br>")
+// document.write(arr.sort((a,b)=>{return b-a}))
--- string_methods.js
+++ string_methods.js
@@ -1,5 +1,56 @@
-let text ="hello world ";
-// let result =text.slice(0,5);
+ let text ="hello world";
+//Length: To get the length of a string, you can use the length property.
+// console.log(text.length)
 
-let result =text.substring(3,8);
-console.log(result)
+//  let result =text.substring(3,8);
+//  console.log(result)
+
+// let res= text.slice(0)
+// let res= text.slice(0,3)
+// let res = text.slice(-5)
+// console.log(res)
+
+// let res = text.substring(6)
+// console.log(res)
+
+// let result =text.substr(4)
+// console.log(result)
+
+// let res =text.replace("world","universe")
+// console.log(res)
+
+// let str = "Mind, Power,Power, Soul";
+// let part = str.replace("Power", "Space");
+// console.log(part);
+
+// let str = "Mind, Power,Power, Soul";
+// let part = str.replaceAll("Power", "Space");
+//  console.log(part);
+
+
+
+// let word="abhishek is pursuing bca from gla university";
+// let new_word=word.toUpperCase();
+// console.log(new_word)
+
+
+//  let str ="ABHISHEK IS PURSUING BCA FROM GLA UNIVERSITY";
+//  let new_word=str.toLowerCase();
+//  console.log(new_word)
+
+// let sen="abhishek is pursuing bca from gla university";
+// let sen2=" and gla university is situated in mathura somewhere ";
+// let sen3=sen.concat(sen2)
+// console.log(sen3)
+
+// let s1= 'ABC    ';
+// let s2 = ' Adam buy Cat  ';
+// console.log(s1.length);
+// console.log(s1.trim())
+// console.log(s2.trim())
+
+// let sen="abhishek is pursuing bca from gla university";
+// console.log(sen.charAt(5))
+// console.log(sen.charCodeAt(15));
+// let arr = sen.split('is')
+// console.log(arr)

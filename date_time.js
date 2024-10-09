ce929aa238b89bfbe12bcf2db428fb5f6fab7835
Trivedi-800 <02trivedi@gmail.com>
2024-10-09 11:51:03 +0530

    date and time in js

---
+++ date_time.js
@@ -0,0 +1,32 @@
+// let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log( typeof mydate)
+
+
+// let myCreatedDate = new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString())
+
+// let mydate=  new Date("2023-00-14")
+// console.log(mydate.toLocaleString()); invalid date
+
+// let mydate=  new Date("10-09-2024")
+// console.log(mydate.toLocaleString())
+
+// console.log(Math.floor(Date.now()/1000))
+
+
+// let MyTimeStamp=Date.now()
+// console.log(MyTimeStamp.)
+
+// let newDate =new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);// for current month 
+// console.log(newDate.getDay());
+
+//  newDate.toLocaleString('default',{
+//     weekday:"long",
+
+//  })

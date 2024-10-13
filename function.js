ffbb044166dacec71899c66f98544096a74bc89e
Trivedi-800 <02trivedi@gmail.com>
2024-10-13 12:10:46 +0530

    function2

--- function.js
+++ function.js
@@ -8,6 +8,15 @@
 // addTwonumbers(55,85.5)//if no val is passed then NaN is output
 // addTwonumbers(55,"a") //here both argumnets are treated as strings
 
+
+
+//  function addTwonumbers(n1,n2){
+//     let result =n1+n2
+//     return result 
+//  }
+//  const result = addTwonumbers(3,5)
+//  console.log("result-", result);
+
 // function newfun(n1,n2){
 //     console.log(n1+n2)
 // }
@@ -15,12 +24,12 @@
 // console.log("Result:",result);
 
 // 
-function loginUserMessage(username){
-    if(username=== undefined){
-        console.log("please enter username");
-        return
-    }
-    return`${username} just logged in`
-}
+// function loginUserMessage(username){
+//     if(username=== undefined){
+//         console.log("please enter username");
+//         return
+//     }
+//     return`${username} just logged in`
+// }
 
-console.log(loginUserMessage("abhi"))
+// console.log(loginUserMessage("abhi"))

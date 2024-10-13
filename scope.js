7646c62f7b11512136e8fbec067e3f06a0c754ff
Trivedi-800 <02trivedi@gmail.com>
2024-10-13 22:28:24 +0530

    scope

---
+++ scope.js
@@ -0,0 +1,49 @@
+// let a =200;// global scope 
+
+// if(true){  //within curlybraces -blockscope
+// let a =10;
+// const b=15;
+// console.log( "inner:" ,a);
+// // function addnum(){
+
+// // }
+// }
+// console.log(a); // not defined 
+//   console.log(b);
+// console.log(c);
+
+// for(let i=0;i<Array.length;i++){
+//     const element = array[i]
+// }
+
+// function one(){
+//   const username="abhishek trivedi";
+
+//   function two(){
+//     const website="youtube" 
+//     console.log(username);
+//    }
+//    //console.log(website);
+//    two()
+// }
+//one()
+// if(true){
+//   const username ="hitesh"
+//   if(username ==="hitesh"){
+//     const website="youtube"
+//     console.log(username + website);
+//   }
+  //console.log(website);
+// }
+//console.log(username);
+
+
+// function addone(num){
+//   return num+1
+// }
+// console.log(addone(5))
+
+const addTwo=function(num){
+return num+2
+}
+console.log(addTwo(5))

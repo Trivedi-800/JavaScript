99b04f9ed72c6a80c85ac1a270527ce896299cf6
Trivedi-800 <02trivedi@gmail.com>
2024-10-13 11:51:57 +0530

    obj

---
+++ objects.js
@@ -0,0 +1,13 @@
+const course ={
+    coursename:"js in hindi",
+    price:"666",
+    CourseInstructor:"Abhishek Trivedi"
+}
+const {CourseInstructor: CI}=course
+console.log(CI);
+
+// {
+//     "name" : "abhishek",
+//     " cname ":"js in hindi",
+//     "price":"free"
+// }

a71680cd5e423d0071112b130d3b4a47e04bb2ec
Trivedi-800 <02trivedi@gmail.com>
2024-10-14 21:53:40 +0530

    arrow function and this keyword

---
+++ arrow_func.js
@@ -0,0 +1,42 @@
+// const user= {
+//     username:"abhishek",
+//     rate: 999,
+//     welcomeMessage :function(){
+//         console.log(`${this.username},welcome to website`);
+//         console.log(this);
+//     }
+
+// }
+// user.welcomeMessage()
+// user.username="ayush"
+// user.welcomeMessage()
+// console.log(this);
+
+// function chai(){
+//     let username="trivedi"
+//     console.log(this.username);
+// }
+// chai() //undefined
+
+// const chai=function(){
+//     let username="trivedi"
+//        console.log(this.username);
+// }
+// chai() //undefined
+
+// const chai=()=>{
+//         let username="trivedi"
+//            console.log(this);
+//     }
+//     chai() //{}
+
+
+// const add=(a,b) => {
+//   return a+b   //explicit return
+// }
+
+// const add=(a,b) =>( a+b)  
+  // console.log(add(4,5)) //implicit return
+
+//   const add=(a,b) =>( {a:222})  
+//    console.log(add(4,5))

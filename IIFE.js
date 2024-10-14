0184a6ace5319fe25296b7ddf91ffa46673966cb
Trivedi-800 <02trivedi@gmail.com>
2024-10-14 22:11:14 +0530

    iife

---
+++ IIFE.js
@@ -0,0 +1,13 @@
+// function chai(){
+//     console.log("dd connected")
+// }
+// chai()
+
+// (function chai(){ //namd iife
+// console.log("database connected")    
+// })();
+// to avoid the pollution of global variables and invoke the function immediately.
+
+// ((name)=>{
+//     console.log(`db connected ${name}`)
+// })("hitesh")

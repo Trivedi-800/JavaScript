b616b68b77c2b68d51140066b524272329134ab3
Trivedi-800 <02trivedi@gmail.com>
2024-10-17 21:37:45 +0530

    conditional Statement

---
+++ if_else.js
@@ -0,0 +1,60 @@
+//if statement
+
+// if(true){
+//     //code executed
+// }
+
+// if (false){
+//     //code will not executed within braces
+// }
+//  const (isUserLoggedIn)=true;
+// if(isUserLoggedIn){
+// //executed
+// }
+// if(3!=2){
+//     //condition is  true so code will be executed
+// }
+//  <,>,<=,=> ,==,===,!=  to check these condtn -
+//-conditional is  used 
+//-------------------------------------
+// const temp=61
+// if(temp<50){
+//     console.log("temperature is less than 50");
+// }
+// else{
+// console.log("greater than 50")};
+//--------------------------------------
+// const score=200;
+// if(score>100){
+// let power="fly";
+//     console.log(`user power:${power}`)
+// }
+// console.log(`user power:${power}`)
+//-------------------------------------
+// const balance=1000
+// if(balance>500) console.log("test"),
+// console.log("test3"); not a good practice avoid this 
+//----------------------------------
+// balance =1000;
+// if(balance <500){
+// console.log("less than 900");
+// }
+// else if(balance<750){
+// console.log("less than 750")
+// }
+// else if (balance<900){
+//     console.log("less than 900")
+//     }
+// else{
+//       console.log("less than 1200")
+//     }
+//----------------------------------------
+// const UserLoggedIn =true
+// const debitCard=true
+// if(UserLoggedIn&&debitCard){
+//     console.log("ALLOW TO BUY COURSE");
+// }
+// else{
+//         console.log("not allowed")
+//     }
+//-------------------------------------

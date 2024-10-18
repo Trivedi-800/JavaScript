31f8c7345458cf9051c63e2256f857f24cef89ba
Trivedi-800 <02trivedi@gmail.com>
2024-10-18 21:44:53 +0530

    truthy and falsy words

---
+++ truthy.js
@@ -0,0 +1,32 @@
+//falsy values 
+//false,0,-0,NaN,null,undefined," ",Big Int 0n
+
+//truthy values
+// "0",'false'," ",[],{},function(){}(values within string)
+// const username="";
+// if(username){
+//     console.log("got user email");
+
+// }
+// else{
+//     console.log("Dont have user email");
+// }
+
+// const emptyobj={
+
+// }
+// if(Object.keys(emptyobj).length===0){
+//     console.log("empty")
+// }
+//nullish coalescing Operator (??):null undefined
+//  let val1;
+// // val1= 5??10 
+// val=null??10??20
+// // val1= undefined??15
+//  console.log(val1)
+
+//ternary operator 
+//Syntax is :--(condition? true:false)
+
+// const iceTeaPrice =100
+// iceTeaPrice>=80?console.log("less than 80"):console.log("more than 80")

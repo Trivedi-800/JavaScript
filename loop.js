0cb3bcb8e5726370e88df9025b0bbcba0e156d02
Trivedi-800 <02trivedi@gmail.com>
2024-10-21 22:44:55 +0530

    loops in js

---
+++ loop.js
@@ -0,0 +1,38 @@
+//for loop-
+// for( let i =0;i<=10;i++ ){
+//     console.log(`outer loop values are ${i}`);
+//     for (let j=0;j<=10;j++){
+//         // console.log|(`inner loop val is ${j} outer inner  loop ${i}`);
+//         // console.log(i+'*'+j+'='+i*j);
+//         // console.log(`innner loop- ${j} and outer loop is- ${i}`)
+//     }
+// }
+
+// let myArray=["flash","batsman","superman"]
+// console.log(myArray.length);
+// for(let index=0;index<myArray.length;index++){
+//     const element =myArray[index];
+//     console.log(element)
+// }
+
+
+//break and continue --------// jump statements 
+//while
+// let i=0;
+//  while(i<10){
+//     console.log(`value of index is ${i}`)
+// i=i+2
+//  }
+
+// let arr=['ram','shyam','sohan','mohan','jaggu']
+// let i=0
+// while(i<arr.length){
+//     console.log(`values are ${arr[i]}`);
+//     i++;
+// }
+//do-while
+// let score=11
+// do{
+// console.log(`score is${score}`);
+// score++;
+// }while(score<=10);

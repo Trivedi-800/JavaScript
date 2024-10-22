f7d5d5885f4b3cc0c597ffed2ca3cc169b42bcaa
Trivedi-800 <02trivedi@gmail.com>
2024-10-22 22:13:51 +0530

    higher order array loops

---
+++ hof.js
@@ -0,0 +1,46 @@
+//for -of 
+// const arr=[1,2,3,4,5]
+// for (const i of arr) {
+//     console.log(i)
+// }
+
+// const greetings ="hello World"
+// for(const greet of greetings){
+//     console.log(`Each Char is ${greet}`)
+// }
+
+//map 
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of America")
+// map.set('FR',"France")
+// map.set('IN',"India")
+// console.log(map)
+// // for (const val of map){
+// //     console.log(val);
+// // }
+// for (const[key,value]  of map){
+    //     console.log(key,':-',value}
+
+//for -in 
+// const obj={
+//     js:'javascript',
+//     cpp:'c plus plus',
+//     rb:'ruby',
+//     swift:'swift by ruby'
+// }
+// for (const key in obj ){
+//     console.log(obj[key]);
+// }
+
+const lang=['js','rb','swift','c++']
+// for (const val in lang){
+//     console.log(lang[val])
+// }
+// lang.forEach( function(i) {
+//     console.log(i)
+// })
+
+// lang.forEach((item,index,arr)=>{
+//     console.log(item,index,arr)
+// })
